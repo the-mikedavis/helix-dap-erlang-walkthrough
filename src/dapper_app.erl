03b1cbf585e019f70e4f0606eea46ca0d939e4ab
Michael Davis <mcarsondavis@gmail.com>
2022-07-02 09:09:49 -0500

    setup a hello world cowboy server
    This is the cowboy hello world example bundled up as a rebar3
    project.
    
    https://github.com/ninenines/cowboy/tree/master/examples/hello_world

--- src/dapper_app.erl
+++ src/dapper_app.erl
@@ -10,9 +10,18 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    Dispatch = cowboy_router:compile([
+        {'_', [
+            {"/", dapper_serve, []}
+        ]}
+    ]),
+    {ok, _} = cowboy:start_clear(http, [{port, 8080}], #{
+        env => #{dispatch => Dispatch}
+    }),
     dapper_sup:start_link().
 
 stop(_State) ->
+    cowboy:stop_listener(http),
     ok.
 
 %% internal functions
---
+++ src/dapper_serve.erl
@@ -0,0 +1,11 @@
+%% @doc Serves all HTTP requests.
+
+-module(dapper_serve).
+
+-export([init/2]).
+
+init(Req0, Opts) ->
+    Req = cowboy_req:reply(200, #{
+        <<"content-type">> => <<"text/plain">>
+    }, <<"Hello from Dapper!">>, Req0),
+    {ok, Req, Opts}.
