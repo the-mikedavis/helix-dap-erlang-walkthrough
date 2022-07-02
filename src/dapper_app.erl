f70f6cf413e4b93c39f1831fe0356c219aae88a7
Michael Davis <mcarsondavis@gmail.com>
2022-07-02 09:12:40 -0500

    run erlang-ls formatter

--- src/dapper_app.erl
+++ src/dapper_app.erl
@@ -10,14 +10,8 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-    Dispatch = cowboy_router:compile([
-        {'_', [
-            {"/", dapper_serve, []}
-        ]}
-    ]),
-    {ok, _} = cowboy:start_clear(http, [{port, 8080}], #{
-        env => #{dispatch => Dispatch}
-    }),
+    Dispatch = cowboy_router:compile([{'_', [{"/", dapper_serve, []}]}]),
+    {ok, _} = cowboy:start_clear(http, [{port, 8080}], #{env => #{dispatch => Dispatch}}),
     dapper_sup:start_link().
 
 stop(_State) ->
--- src/dapper_serve.erl
+++ src/dapper_serve.erl
@@ -5,7 +5,8 @@
 -export([init/2]).
 
 init(Req0, Opts) ->
-    Req = cowboy_req:reply(200, #{
-        <<"content-type">> => <<"text/plain">>
-    }, <<"Hello from Dapper!">>, Req0),
+    Req = cowboy_req:reply(200,
+                           #{<<"content-type">> => <<"text/plain">>},
+                           <<"Hello from Dapper!">>,
+                           Req0),
     {ok, Req, Opts}.
