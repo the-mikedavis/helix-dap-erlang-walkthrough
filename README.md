fd0f5d99afc4cbd53700907f111cb6556bf463c4
Michael Davis <mcarsondavis@gmail.com>
2022-07-02 08:54:26 -0500

    rebar3 new app dapper

---
+++ README.md
@@ -0,0 +1,9 @@
+dapper
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ src/dapper_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc dapper public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(dapper_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    dapper_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
